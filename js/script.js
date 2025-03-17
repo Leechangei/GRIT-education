09baf5ef2a67dabd3d375ab6afbf1f3984ec69f6
Leechangei <dlckddml01@gmail.com>
2025-03-17 09:48:48 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("학원 홈페이지가 로드되었습니다.");
+
+  // header.html 파일을 가져와서 #header-container에 삽입
+  fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header-container").innerHTML = data;
+    });
 });
